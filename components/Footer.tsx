3e7aaf56bc1808fa18e2285fdbe9d1dbd1fefdd2
Bisimwa Grace <bisimwagrace596@gmail.com>
2024-08-28 06:00:37 +0200

    Self closing div

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <img
           src='/footer-grid.svg'
           alt='grid'
-          className='w-full h-full opacity-50 '
+          className='w-full h-full opacity-50'
         />
       </div>
 
